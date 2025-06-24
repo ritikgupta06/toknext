7dc7afb01f989f616ba89a428904f011006a215f
ritikgupta06 <142598621+ritikgupta06@users.noreply.github.com>
2025-06-24 22:01:12 +0530

    backend initiated

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export function signup(req, res) {
+  res.send("SignUP");
+}
+export function login(req, res) {
+  res.send("LogIn");
+}
+export function logout(req, res) {
+  res.send("LogOut");
+}
---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import authRouths from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use("/api/auth/", authRouths);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+  connectDB();
+});
