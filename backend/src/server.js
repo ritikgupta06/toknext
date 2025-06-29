a08566919174d4cabe8492a892732998b542e632
ritikgupta06 <142598621+ritikgupta06@users.noreply.github.com>
2025-06-29 22:39:06 +0530

    project completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,9 +7,12 @@ import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -24,6 +27,13 @@ app.use("/api/auth/", authRoutes);
 app.use("/api/users/", userRoutes);
 app.use("/api/chats/", chatRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
   connectDB();
